19e212dbf07581893e8c23cf40a5c521b9c840bb
BryantChang <finish740@sina.com>
2017-11-26 12:09:25 +0800

    modify files

--- conf/env.sh
+++ conf/env.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
+# ##Log locations
+# EXECUTOR_ORI_LOG_DIR="/Users/bryantchang/Desktop/study/Research/experiment/original_log/sparklogs_1114/executor_logs"
+# MONITOR_ORI_LOG_DIR="/Users/bryantchang/Desktop/study/Research/experiment/original_log/sparklogs_1114/monitor_logs"
+# MONITOR_RES_LOG_DIR="/Users/bryantchang/Desktop/study/Research/experiment/original_log/sparklogs_1114/monitor_logs/analyse_result"
+# GC_ORI_LOG_DIR="/Users/bryantchang/Desktop/study/Research/experiment/original_log/sparklogs_1114/gc_logs"
+# GC_RES_LOG_DIR="/Users/bryantchang/Desktop/study/Research/experiment/original_log/sparklogs_1114/gc_logs/gc_result"
+
 ##Log locations
-EXECUTOR_ORI_LOG_DIR="/Users/bryantchang/Desktop/study/Research/experiment/original_log/sparklogs_1114/executor_logs"
-MONITOR_ORI_LOG_DIR="/Users/bryantchang/Desktop/study/Research/experiment/original_log/sparklogs_1114/monitor_logs"
-MONITOR_RES_LOG_DIR="/Users/bryantchang/Desktop/study/Research/experiment/original_log/sparklogs_1114/monitor_logs/analyse_result"
-GC_ORI_LOG_DIR="/Users/bryantchang/Desktop/study/Research/experiment/original_log/sparklogs_1114/gc_logs"
-GC_RES_LOG_DIR="/Users/bryantchang/Desktop/study/Research/experiment/original_log/sparklogs_1114/gc_logs/gc_result"
-
-
-
+EXECUTOR_ORI_LOG_DIR="/home/hadoop/bryantchang/logs/sparklogs/executor_logs"
+MONITOR_ORI_LOG_DIR="/home/hadoop/bryantchang/logs/sparklogs/monitor_logs"
+MONITOR_RES_LOG_DIR="/home/hadoop/bryantchang/logs/sparklogs/monitor_logs/analyse_result"
+GC_ORI_LOG_DIR="/home/hadoop/bryantchang/logs/sparklogs/gc_logs"
+GC_RES_LOG_DIR="/home/hadoop/bryantchang/logs/sparklogs/gc_logs/gc_result"
 
 
 
