9a0b19d50554a11534405639b8c265d3a229bab6
BryantChang <finish740@sina.com>
2017-12-25 10:16:14 +0800

    modify files

--- conf/sysenv.sh
+++ conf/sysenv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-TOOLS_SCRIPT_HOME="/home/zc/bryantchang/Linux-Scripts/System_scripts/ssh"
-#SCRIPT_HOME="/home/hadoop/bryantchang/tools/Scripts/System_scripts/ssh"
+#TOOLS_SCRIPT_HOME="/home/zc/bryantchang/Linux-Scripts/System_scripts/ssh"
+TOOLS_SCRIPT_HOME="/home/hadoop/bryantchang/tools/Scripts/System_scripts/ssh"
 SAMPLE_COUNT=75
 MEM_STEP=200
