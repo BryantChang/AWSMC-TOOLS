554aeb3fe356dde122498ab865f4592e7792be75
BryantChang <finish740@sina.com>
2017-12-25 10:06:59 +0800

    modify files

--- conf/env.sh
+++ conf/env.sh
@@ -17,42 +17,42 @@
 #GC_RES_LOG_DIR="/Users/bryantchang/Desktop/study/Research/experiment/for_remote/sparklogs1126/sparklogs/gc_logs/gc_result"
 
 
-###Spark config
-#SPARK_HOME="/home/hadoop/bryantchang/platforms/Spark_Master"
-#SPARKBENCH_HOME="/home/hadoop/bryantchang/codes/SparkBench_ml"
-
-###Log locations
-#EXECUTOR_ORI_LOG_DIR="/home/hadoop/bryantchang/logs/sparklogs/executor_logs"
-#MONITOR_ORI_LOG_DIR="/home/hadoop/bryantchang/logs/sparklogs/monitor_logs"
-#MONITOR_RES_LOG_DIR="/home/hadoop/bryantchang/logs/sparklogs/monitor_logs/analyse_result"
-#GC_ORI_LOG_DIR="/home/hadoop/bryantchang/logs/sparklogs/gc_logs"
-#GC_RES_LOG_DIR="/home/hadoop/bryantchang/logs/sparklogs/gc_logs/gc_result"
-#
-#
-###internal result
-#TMP_DIR="/home/hadoop/bryantchang/logs/tmp"
-#ERR_DIR="/home/hadoop/bryantchang/logs/error"
-#EVENTLOG_DIR="/home/hadoop/bryant/logs/eventlogs"
-
-
 ##Spark config
-SPARK_HOME="/home/zc/bryantchang/srcs/Spark_Master"
-SPARKBENCH_HOME="/home/zc/bryantchang/SparkBench"
-
-
+SPARK_HOME="/home/hadoop/bryantchang/platforms/Spark_Master"
+SPARKBENCH_HOME="/home/hadoop/bryantchang/codes/SparkBench_ml"
 
 ##Log locations
-EXECUTOR_ORI_LOG_DIR="/home/zc/bryantchang/logs/executor_logs"
-MONITOR_ORI_LOG_DIR="/home/zc/bryantchang/logs/monitor_logs"
-MONITOR_RES_LOG_DIR="/home/zc/bryantchang/logs/monitor_logs/analyse_result"
-GC_ORI_LOG_DIR="/home/zc/bryantchang/logs/gc_logs"
-GC_RES_LOG_DIR="/home/zc/bryantchang/logs/gc_logs/gc_result"
+EXECUTOR_ORI_LOG_DIR="/home/hadoop/bryantchang/logs/sparklogs/executor_logs"
+MONITOR_ORI_LOG_DIR="/home/hadoop/bryantchang/logs/sparklogs/monitor_logs"
+MONITOR_RES_LOG_DIR="/home/hadoop/bryantchang/logs/sparklogs/monitor_logs/analyse_result"
+GC_ORI_LOG_DIR="/home/hadoop/bryantchang/logs/sparklogs/gc_logs"
+GC_RES_LOG_DIR="/home/hadoop/bryantchang/logs/sparklogs/gc_logs/gc_result"
 
 
 ##internal result
-TMP_DIR="/home/zc/bryantchang/logs/tmp"
-ERR_DIR="/home/zc/bryantchang/logs/error"
-EVENTLOG_DIR="/home/zc/bryantchang/logs/eventlogs"
+TMP_DIR="/home/hadoop/bryantchang/logs/tmp"
+ERR_DIR="/home/hadoop/bryantchang/logs/error"
+EVENTLOG_DIR="/home/hadoop/bryant/logs/eventlogs"
+
+
+###Spark config
+#SPARK_HOME="/home/zc/bryantchang/srcs/Spark_Master"
+#SPARKBENCH_HOME="/home/zc/bryantchang/SparkBench"
+#
+#
+#
+###Log locations
+#EXECUTOR_ORI_LOG_DIR="/home/zc/bryantchang/logs/executor_logs"
+#MONITOR_ORI_LOG_DIR="/home/zc/bryantchang/logs/monitor_logs"
+#MONITOR_RES_LOG_DIR="/home/zc/bryantchang/logs/monitor_logs/analyse_result"
+#GC_ORI_LOG_DIR="/home/zc/bryantchang/logs/gc_logs"
+#GC_RES_LOG_DIR="/home/zc/bryantchang/logs/gc_logs/gc_result"
+#
+#
+###internal result
+#TMP_DIR="/home/zc/bryantchang/logs/tmp"
+#ERR_DIR="/home/zc/bryantchang/logs/error"
+#EVENTLOG_DIR="/home/zc/bryantchang/logs/eventlogs"
 
 
 
